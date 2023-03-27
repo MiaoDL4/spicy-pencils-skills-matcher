f02d2166e6b7f686718a1182f896ba24c359f8a7
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-03-27 19:23:46 +1030

    added the match code into new version

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -135,4 +135,79 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+
+
+//===== match page============//
+
+router.get('/matches', withAuth, async (req, res) => {
+  try {
+    const teachData = await Teach.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const learnData = await Learn.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const canTeach = teachData.map((teach) => teach.get({ plain: true }));
+    const canLearn = learnData.map((learn) => learn.get({ plain: true }));
+
+    console.log(canTeach[0].name);
+
+    let sortedCSSObj = [];
+    let sortedHTMLObj = [];
+    let sortedJSObj = [];
+
+
+    for (let i = 0; i < canTeach.length; i++) {
+      if (canTeach[i].name === "CSS" && canLearn[i].name === "CSS") {
+        sortedCSSObj.push(canTeach[i]);
+        sortedCSSObj.push(canLearn[i]);
+      } else if (canTeach[i].name === "HTML" && canLearn[i].name === "HTML") {
+        sortedHTMLObj.push(canTeach[i]);
+        sortedHTMLObj.push(canLearn[i]);
+      } else {
+        sortedJSObj.push(canTeach[i]);
+        sortedJSObj.push(canLearn[i]);
+      }
+    }
+    console.log('================================================');
+    console.log(sortedCSSObj);
+    console.log('================================================');
+    console.log(sortedHTMLObj);
+    console.log('================================================');
+    console.log(sortedJSObj);
+    console.log('================================================');
+
+  
+    res.render('match', {
+      sortedCSSObj,
+      sortedHTMLObj,
+      sortedJSObj,
+      logged_in: req.session.logged_in,
+    });
+    console.log(req.session.logged_in);
+    console.log('================================================');
+    console.log(sortedCSSObj);
+    console.log('================================================');
+    console.log(sortedHTMLObj);
+    console.log('================================================');
+    console.log(sortedJSObj);
+    console.log('================================================');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
